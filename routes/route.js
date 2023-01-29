389c0b73d0f543c8a160ecd677e188c2e32ba03c
EUNICE JACOB <jacobeunice40@gmail.com>
2023-01-29 01:44:29 +0100

    crete route file to manage different routes.

---
+++ routes/route.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { usercontrols } = require('../controllers/userControls');
+
+const routemanager = express.Router();
+
+routemanager.get('/', (req, res) => {
+  res.render('index');
+});
+
+routemanager.get('/admin', (req, res) => {
+  res.render('pages/admin');
+});
+
+routemanager.get('/login', (req, res) => {
+  res.render('pages/login');
+});
+
+routemanager.get('/signup', (req, res) => {
+  res.render('pages/signup');
+});
+
+module.exports = { routemanager };
